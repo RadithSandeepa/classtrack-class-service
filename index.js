8f26861425a20e91f5acefdcf4d7e5afe5c71b6d
RadithSandeepa <radithsandeepa500@gmail.com>
2026-04-19 07:35:17 +0530

    chore: Update database connection to use Cloud SQL socket path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import scheduleRoutes from './routes/scheduleRoutes.js';
 import attendanceRoutes from './routes/attendanceRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
